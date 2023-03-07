7b1acb6064684790b865e63d1d42dea10bcdc9be
johansson017 <oliver.johansson.jkp@gmail.com>
2023-03-07 21:49:29 +0100

    Fixed script

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,20 @@
+""" This is a small script """
 
-#!/usr/bin/python3
-
-
-main():
+def main():
     print("Hello, World!")
 
+class Printables:
+    def __init__(self, default="Hello, World!"):
+        self.default = default
+    
+    def personalize(self, person):
+        self.default = f"Hello, {person}!"
 
+    def __str__(self):
+        return self.default
 
-if "__name__"==__main__:
+if __name__=="__main__":
     main()
-
 
 
 
